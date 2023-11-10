0963a15412e2d60c088cc4e447e37227715dc715
saisowje <75520548+saisowje@users.noreply.github.com>
2023-11-10 14:01:00 +0530

    Add files via upload

---
+++ 10-11task python.py
@@ -0,0 +1,69 @@
+fruits = ['apple','banana','cherry']
+fruits.append('orange')
+print(fruits)
+
+
+numbers = [1,2,3,4,5]
+numbers.clear()
+print(numbers)
+
+dry_fruits  = ['angeer','almond','kaju','walnut']
+s = dry_fruits.copy()
+print(s)
+
+
+fruits = ["kiwi", "cherry", "guva"]
+
+x = fruits.count("cherry")
+
+print(x)
+
+
+bikes = ['scooty pep', 'vespa', 'yamaha']
+
+cars = ['Ford', 'BMW', 'Volvo']
+
+bikes.extend(cars)
+
+print(bikes)
+
+fruits = ['apple', 'banana', 'cherry']
+
+x = fruits.index("cherry")
+
+print(x)
+
+names = ['sowje', 'ravi', 'lakshmi']
+
+names.insert(2, "yashu")
+
+print(names)
+
+citys = ['hyderabad', 'chennai', 'banglore']
+
+citys.pop(1)
+
+print(citys)
+
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.remove("banana")
+
+print(fruits)
+
+flowers = ['lily', 'tulip', 'rose']
+
+flowers.reverse()
+
+print(flowers)
+
+
+cars = ['Ford', 'BMW', 'Volvo']
+
+cars.sort()
+
+print(cars)
+
+list_a = [5, 4, 3, 2, 1]
+list_b = list_a[-3:-1]
+print(list_b)
