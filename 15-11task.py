dd9ca3d5e9acbc8531dc45cf72fc5cdbf37b124e
sowje <75520548+saisowje@users.noreply.github.com>
2023-11-15 11:32:21 +0530

    Add files via upload

---
+++ 15-11task.py
@@ -0,0 +1,23 @@
+# main_program.py
+# math_operations.py
+
+def add_numbers(a, b):
+    return a + b
+
+def subtract_numbers(a, b):
+    return a - b
+
+def multiply_numbers(a, b):
+    return a * b
+
+fr'math_operations''add_numbers'
+
+def main():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    result = add_numbers(num1, num2)
+    print(f"The sum of {num1} and {num2} is: {result}")
+
+if _name_ == "_main_":
+    main()
